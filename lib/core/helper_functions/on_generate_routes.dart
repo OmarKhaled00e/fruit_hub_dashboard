646331e4d8b915040ba480f2a4c1d6eb9f65ffc7
OmarKhaled00e0 <ok4966541@gmail.com>
2026-01-12 20:46:02 +0200

    add firebase in project

---
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub_dashboard/features/dashboard/views/dashboard_view.dart';
+
+Route<dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case DashboardView.routeName:
+      return MaterialPageRoute(builder: (context) => DashboardView());
+    default:
+      return MaterialPageRoute(builder: (context) => Scaffold());
+  }
+}
